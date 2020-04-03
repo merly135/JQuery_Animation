062a37912c26235e65806e3e8fe8f6cf85805b59
Merly135 <merly135@outlook.com>
2020-04-03 13:35:42 -0700

    Update

--- Animation.js
+++ Animation.js
@@ -1,1 +1,48 @@
-$
+$(".stop").click(function () {
+    $(".pressDown").stop(loop);
+});
+
+$(".pressDown").dblclick(loop)
+
+function loop() {
+    $("#pic1")
+        .animate({
+            width: "10%"
+        })
+        .animate({
+            width: "50%"
+        }, 700)
+        .animate({
+            opacity: "50%"
+        }, 350)
+        .animate({
+            opacity: "100%"
+        }, 350)
+    $("#pic2")
+        .animate({
+            width: "10%"
+        })
+        .animate({
+            width: "50%"
+        }, 700)
+        .animate({
+            opacity: "50%"
+        })
+        .animate({
+            opacity: "100%"
+        })
+
+    $("#pic3")
+        .animate({
+            width: "10%"
+        })
+        .animate({
+            width: "50%"
+        }, 700)
+        .animate({
+            opacity: "50%"
+        }, 350)
+        .animate({
+            opacity: "100%"
+        }, 350, loop)
+}
